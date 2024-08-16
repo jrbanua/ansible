fd8b21153a01177966173eea27bc19373f3f0045
Jack Raymund Banua <raxjackass14@gmail.com>
2024-08-16 09:36:45 -0400

    change bash and playbook script

--- files/change-background.sh
+++ files/change-background.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cd /etc/dconf/db/local.d/locks
-sudo rm background
-dconf write /org/gnome/desktop/background/picture-uri "'file:///usr/share/backgrounds/Wonders_Wallpaper.png'"
+dconf write /org/gnome/desktop/background/picture-uri "'file:///usr/share/backgrounds/linuxtest.jpg'"
